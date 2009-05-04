af5ae1b2ed2b9a90b67c842e17e29224ce04f8b3
ajselvig <ajselvig@142920a8-ac49-0410-9339-5d950d6e6b25>
2009-05-04 04:03:50 +0000

    Beginning implementation of spline.

---
+++ monoworks/Modeling/Sketching/SplineSketcher.cs
@@ -0,0 +1,45 @@
+﻿// SplineSketcher.cs - MonoWorks Project
+//
+//  Copyright (C) 2009 Andy Selvig
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+
+using System;
+using System.Collections.Generic;
+
+using gl = Tao.OpenGl.Gl;
+
+using MonoWorks.Base;
+using MonoWorks.Rendering;
+using MonoWorks.Framework;
+using MonoWorks.Rendering.Events;
+
+namespace MonoWorks.Modeling.Sketching
+{
+
+	/// <summary>
+	/// Sketcher for splines.
+	/// </summary>
+	public class SplineSketcher : LineSketcher
+	{
+
+		public SplineSketcher(Spline spline) : base(spline)
+		{
+			
+		}
+
+
+	}
+}
