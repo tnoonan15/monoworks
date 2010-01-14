070ee71ca6f2536ace73f5abc47b7023a02d7827
ajselvig <ajselvig@142920a8-ac49-0410-9339-5d950d6e6b25>
2010-01-14 01:57:12 +0000

    Renamed all the Gtk stuff with the new naming convention. Added some more icons to the demo.

--- trunk/monoworks/Controls/DecoratorBase.cs
+++ trunk/monoworks/Controls/DecoratorBase.cs
@@ -61,7 +61,7 @@ namespace MonoWorks.Controls
 				new Color(0.5f, 0.5f, 0.5f),
 				new Color(0.5f, 0.5f, 0.5f),
 				new Color(0.6f, 0.6f, 0.6f),
-				new Color(0f, 0.5f, 0f)
+				new Color(0.3f, 0.5f, 0.3f)
 			));
 			
 			SetColorGroup(ColorType.EditableStart,
@@ -80,7 +80,7 @@ namespace MonoWorks.Controls
       			new Color(0.9f, 1f, 0.9f, 0.9f)
             ));
 			
-			SelectionColor = new Color(0.2f, 0.4f, 1f, 0.5f);
+			SelectionColor = new Color(0.4f, 0.6f, 1f, 0.5f);
 		}
 		
 		/// <value>
