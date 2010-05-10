6fef099725f885f210f594191f5b48c4594f61c8
ajselvig <ajselvig@142920a8-ac49-0410-9339-5d950d6e6b25>
2010-05-10 02:58:44 +0000

    added some wpf stuff back in since svn screwed it

---
+++ monoworks/Wpf/Demo/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+namespace DemoWpf
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+
+		public App()
+			: base()
+		{
+		}
+
+	}
+}
---
+++ monoworks/Wpf/Demo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using MonoWorks.Controls;
+using MonoWorks.Demo;
+using MonoWorks.WpfBackend;
+using MonoWorks.Rendering;
+using MonoWorks.Plotting;
+using MonoWorks.Modeling;
+
+namespace MonoWorks.DemoWpf
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			// create the scene book
+			var sceneSpace = new SceneSpace(viewportWrapper.Viewport);
+			viewportWrapper.Viewport.RootScene = sceneSpace;
+			var book = new SceneBook(viewportWrapper.Viewport);
+			sceneSpace.Root = book;
+
+			// create the controls scene
+			var controls = new ControlsScene(viewportWrapper.Viewport);
+			book.Add(controls);
+
+			// create the 2D plotting scene
+			var plot2D = new Plot2dScene(viewportWrapper.Viewport);
+			book.Add(plot2D);
+
+			// create the 3D plotting scene
+			var plot3D = new Plot3dScene(viewportWrapper.Viewport);
+			book.Add(plot3D);
+		}
+
+
+	}
+}
